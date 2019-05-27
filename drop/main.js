77ab81d1411f11fb8898825a10fd4a116ab78c9d
ihor <igor@example.com>
2019-05-27 23:21:03 +0300

    refactor

--- drop/main.js
+++ drop/main.js
@@ -10,48 +10,31 @@ fetch('./data.json')
     data.forEach(el => {
       let option = document.createElement("option");
       option.text = el.name;
-      selectCountries.appendChild(option);
+      selectCountries.insertBefore(option, null)
     });
     
     let currentCountry = null;
-
+    
     selectCountries.addEventListener("change", function(e){
-        selectCities.innerHTML = "";
-        selectStreet.innerHTML = "";
-        // не думаю що це правильно писати лишній код 
-        defOp = document.createElement("option");
-        defOp.text = "choose some city";
-        selectCities.appendChild(defOp);
-        // не думаю що це правильно писати лишній код 
-        defOp1 = document.createElement("option");
-        defOp1.text = "choose some streets";
-        selectStreet.appendChild(defOp1);
-        if(!currentCountry){
-          selectCities.disabled == true;
-          selectStreet.disabled == true;
-        }
+        selectCities.options.length = 1;
+        selectStreet.options.length = 1;
 
         currentCountry = data.find(country => country.name === e.target.value);
         currentCountry.cities.forEach(el => {
             let option = document.createElement("option");
             option.text = el.name;
-            selectCities.appendChild(option);
+            selectCities.insertBefore(option, null)
             selectCities.disabled = false;
         });
     });
     
     selectCities.addEventListener('change', function(e){
       const city = currentCountry.cities.find(cities => cities.name === e.target.value);
-      selectStreet.innerHTML = "";
-      // не думаю що це правильно писати лишній код 
-      defOp1 = document.createElement("option");
-      defOp1.text = "choose some streets";
-      selectStreet.appendChild(defOp1);
-      
+      selectStreet.options.length = 1;
       city.streets.forEach(el => {
           let option = document.createElement("option");
           option.text = el.name;
-          selectStreet.appendChild(option);
+          selectStreet.insertBefore(option, null)
           selectStreet.disabled = false;
         });
     });
