41576ea1fdf304cd119103b77bb3e0734fad394f
ihor <igor@example.com>
2019-06-06 17:54:23 +0300

    improve data transfer

--- drop/src/fetch.js
+++ drop/src/fetch.js
@@ -1,23 +1,18 @@
-import dataOne from "./data.json"
-let cuntriesName = dataOne;
+let countriesName;
 let cityArray = null;
 let streetArray = [];
 
 fetch("./src/data.json")
     .then(res => res.json())
     .then(data => {
+        getCountries(data);
         getCity(data);
         getStreet(data);
     }).catch(error => {
         console.log("Somthing went wrong" + error)
     });
-
-const getCity = data => {
-    cityArray = data.filter(country => country.cities);
-    return cityArray;
-}
-const getStreet = data => {
-   return data.filter(el => streetArray.push(el))
-}
-
-export { cuntriesName, cityArray, streetArray }
+const getCountries = data =>  countriesName = data;
+const getCity = data => cityArray = data.filter(country => country.cities);
+const getStreet = data => data.filter(el => streetArray.push(el));
+ 
+export { countriesName, cityArray, streetArray }
