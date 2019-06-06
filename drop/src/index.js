c14252f5a2665f8c0850f2eb5f172850967db187
ihor <igor@example.com>
2019-06-06 17:58:49 +0300

    fix fill streets select

--- drop/src/index.js
+++ drop/src/index.js
@@ -1,26 +1,28 @@
 import { addOption, fillCountriesSelect } from "./dataFillContent";
 import { disabledOtherDropdown } from "./disabledDropdown";
 import {cleanOptions} from "./cleanOption";
-import  {cuntriesName, cityArray, streetArray } from "./fetch"
+import  { countriesName, cityArray, streetArray } from "./fetch"
 
 const countries = document.querySelector('#countries');
 const cities = document.querySelector('#cities');
 const streets = document.querySelector('#streets');
 let currentCountry;
-
-fillCountriesSelect(cuntriesName, countries);
+setTimeout(() => {
+  fillCountriesSelect(countriesName, countries);
+},1000);
 
 countries.addEventListener('change', event => {
   cleanOptions(cities, streets);
   currentCountry = cityArray.find(country => country.name === event.target.value);
   disabledOtherDropdown(currentCountry, cities, streets);
   currentCountry.cities.forEach(city => addOption(cities, city.name));
-  
 });
 
 cities.addEventListener('change', event => {
   cleanOptions(streets, streets);
-  const city = currentCountry.cities.find(cityName => cityName.name === event.target.value);
-  //const city = streetArray[0].cities.find(cityName => cityName.name === event.target.value);
-  city.streets.forEach(street => addOption(streets, street.name));
+  streetArray.forEach(el => el.cities.filter(cityName => {
+    if(cityName.name === event.target.value){
+      cityName.streets.forEach(street => addOption(streets, street.name))
+    }
+  }));
 });
