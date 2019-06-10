671efde576787862d8e9a5254282c5bb26d5934d
ihor <igor@example.com>
2019-06-10 21:50:32 +0300

    improve forEach

--- drop/src/dataFillContent.js
+++ drop/src/dataFillContent.js
@@ -1,10 +1,6 @@
 import _ from "lodash"
 
 export const addOption = (select, val) => {
-  // const o = document.createElement('option');
-  // o.innerText = "Loading..."
-  // select.parentNode
-  
   const opt = document.createElement('option');
   opt.innerText = val;
   select.appendChild(opt);
@@ -12,7 +8,5 @@ export const addOption = (select, val) => {
 };
 
 export const fillCountriesSelect = (data, countries) => {
-  _.forEach(data, country => {
-    addOption(countries, country.name);
-  })
+  _.forEach(data, country => addOption(countries, country.name))
 };
